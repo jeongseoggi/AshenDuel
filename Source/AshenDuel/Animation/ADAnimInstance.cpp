6e08cb133c38127c132b416960cea51cd7e30cff
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-06-08 20:46:54 +0900

    [Update] : 모션 매칭에 따라 움직이는 애니메이션 추가

---
+++ Source/AshenDuel/Animation/ADAnimInstance.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ADAnimInstance.h"
+
+#include "AshenDuel/CoreFramework/Character/ADCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UADAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	OwnerCharacter = Cast<AADCharacter>(TryGetPawnOwner());
+	
+	if (OwnerCharacter)
+	{
+		OwnerMovementComp = OwnerCharacter->GetCharacterMovement();
+	}
+}
+
+void UADAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (IsValid(OwnerCharacter))
+	{
+		Velocity = OwnerMovementComp->Velocity;
+		GroundSpeed = FVector(Velocity.X, Velocity.Y, 0).Size();
+		bHasAcceleration = !OwnerMovementComp->GetCurrentAcceleration().IsNearlyZero();
+		bShouldMove = bHasAcceleration;
+		bIsFalling = OwnerMovementComp->IsFalling();
+		Direction = CalculateDirection(Velocity, OwnerCharacter->GetActorRotation());
+	}
+	
+}
---
+++ Source/AshenDuel/Animation/ADAnimInstance.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ADAnimInstance.generated.h"
+
+class UCharacterMovementComponent;
+class AADCharacter;
+/**
+ * 
+ */
+UCLASS()
+class ASHENDUEL_API UADAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+protected:
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	TObjectPtr<AADCharacter> OwnerCharacter;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	TObjectPtr<UCharacterMovementComponent> OwnerMovementComp;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	FVector Velocity;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	float GroundSpeed;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	bool bShouldMove;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	bool bHasAcceleration;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	bool bIsFalling;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	float Direction;
+	
+};
